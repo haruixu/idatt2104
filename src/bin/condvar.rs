5f1d79cb362c17ce460786aca3e23d09acd3c9a0
haruixu <xulr0820@hotmail.com>
2024-01-30 19:16:22 +0100

    Wrap task in message enum to allow terminating message to be sent that prevent indefinite blocking on condvar

--- src/bin/condvar.rs
+++ src/bin/condvar.rs
@@ -6,43 +6,52 @@ fn main() {
     let mut worker_threads = Workers::new(4); //Create 4 internal threads
     let mut event_loop = Workers::new(1); //Create 1 internal thread
 
-    worker_threads.post(Box::new(|| {
+    worker_threads.post(Message::NewTask(Box::new(|| {
         let mut sum = 0;
         for i in 0..10 {
             sum += i;
         }
         println!("Sum: {sum}");
-    }));
+    })));
 
-    /*worker_threads.post(Box::new(|| {
+    worker_threads.post(Message::NewTask(Box::new(|| {
         let mut sum = 0;
         for i in 0..20 {
             sum += i;
         }
         println!("Sum: {sum}");
-    }));
+    })));
 
-    event_loop.post(Box::new(|| {
+    event_loop.post(Message::NewTask(Box::new(|| {
         let mut sum = 0;
         for i in 0..30 {
             sum += i;
         }
         println!("Sum: {sum}");
-    }));
+    })));
 
-    event_loop.post(Box::new(|| {
+    event_loop.post(Message::NewTask(Box::new(|| {
         let mut sum = 0;
         for i in 0..40 {
             sum += i;
         }
         println!("Sum: {sum}");
-    }));*/
+    })));
 
-    worker_threads.join();
+    println!("Main done");
 
-    println!("falling out of scope");
+    std::thread::sleep(Duration::from_secs(3));
+    //todo calle på stop her!!!!!!
 }
 
+//Sendng terminate-message equal to the amount of threads means that each thread is guranteed to
+//finish because we're stacking on new tasks which will wake up waiters. This way we prevent
+//threads from waiting indefinitely on a empty task queue, because the thread unfortunately missed
+//the cv-signal
+enum Message {
+    NewTask(Task),
+    Terminate,
+}
 //Declare a type for the task to avoid syntax cluttering
 type Task = Box<dyn FnOnce() + Send + 'static>;
 
@@ -56,14 +65,13 @@ type Task = Box<dyn FnOnce() + Send + 'static>;
 /// Is_finished flag is used to notify threads when to break loop
 /// Saw that you maybe could you condvar - broadcast (but gpt suggestion).
 struct Workers {
-    tasks: Arc<(Mutex<Vec<Task>>, Condvar)>,
-    threads: Vec<thread::JoinHandle<()>>, //Need to store thread somewhere
-    is_finished: bool,
+    tasks: Arc<(Mutex<Vec<Message>>, Condvar)>,
+    threads: Vec<Option<thread::JoinHandle<()>>>, //Need to store thread somewhere
 }
 
 impl Workers {
     fn new(nr_workers: usize) -> Self {
-        let tasks = Arc::new((Mutex::new(Vec::<Task>::new()), Condvar::new()));
+        let tasks = Arc::new((Mutex::new(Vec::<Message>::new()), Condvar::new()));
         let mut threads = Vec::with_capacity(nr_workers);
         let is_finished = false;
 
@@ -71,40 +79,38 @@ impl Workers {
             let tasks = Arc::clone(&tasks);
 
             let thread = thread::spawn(move || {
-                println!("Creating thread");
                 while !is_finished {
                     let (lock, cvar) = &*tasks;
 
                     let mut task_queue = lock.lock().unwrap();
 
                     while task_queue.is_empty() {
-                        println!("Waiting");
                         task_queue = cvar.wait(task_queue).unwrap();
                     }
 
-                    println!("Finished waiting");
-
                     //Assume none-option never occurs
-                    let task = task_queue.pop().unwrap();
+                    let message = task_queue.pop().unwrap();
 
-                    //Releasing lock before running task
-                    drop(lock);
-                    task();
+                    match message {
+                        Message::NewTask(task) => {
+                            println!("Starting");
+                            //Releasing lock before running task
+                            drop(lock);
+                            task();
+                        }
+
+                        Message::Terminate => break,
+                    }
                 }
             });
-            println!("pushing thread to threads");
-            threads.push(thread);
+            threads.push(Some(thread));
         }
 
-        Workers {
-            tasks,
-            threads,
-            is_finished,
-        }
+        Workers { tasks, threads }
     }
 
-    fn post(&self, task: Task) {
-        println!("Creating task");
+    fn post(&self, task: Message) {
+        println!("Creating new task");
         let (lock, cvar) = &*self.tasks;
 
         let mut queue = lock.lock().unwrap();
@@ -114,44 +120,38 @@ impl Workers {
     }
 
     fn stop(&mut self) {
-        self.is_finished = true;
         let (lock, cv) = &*self.tasks;
-        let mut queue = lock.lock().unwrap();
-        queue.clear();
-        println!("Just cleared tasks");
-
-        cv.notify_all();
+        for _ in &mut self.threads {
+            let mut queue = lock.lock().unwrap();
+            queue.push(Message::Terminate);
+            cv.notify_one();
+        }
     }
 
-    fn post_timeout(&self, task: Task, delay: u64) {
+    fn post_timeout(&self, task: Message, delay: u64) {
         thread::sleep(Duration::from_millis(delay));
         Workers::post(self, task);
     }
-
-    fn join(&mut self) {
-        //While is_finished = false - vent på en cv for is_finished som notifyer når vecen er tom i
-        //thread metoden?????
-        println!("Calling jion");
-        Workers::stop(self);
-
-        println!("isfinished: {}", self.is_finished);
-        let queue = std::mem::take(&mut self.threads);
-        for thread in queue {
-            let _ = thread.join().unwrap();
-        }
-    }
 }
 
-//Not really doing anything here, because dropping is essentially handled by join, but nice
-//having tried to implement at least once
+//Not really doing anything here, because dropping is essentially handled by join, but nice having tried to implement at least once
 impl Drop for Workers {
     fn drop(&mut self) {
-        println!("dropping + is_finished={}", self.is_finished);
-        Workers::stop(self);
-
-        let queue = std::mem::take(&mut self.threads);
-        for thread in queue {
-            let _ = thread.join();
+        {
+            //Pushing terminate-tasks to task-vec with amount equal to amount of threads, that way we guarantee that all threads break the
+            //while loop, because they won't be waiting indefinitely on the condvar signal.
+            //Notify to wake up already waiting threads
+            let (lock, cv) = &*self.tasks;
+            for _ in &mut self.threads {
+                let mut queue = lock.lock().unwrap();
+                queue.push(Message::Terminate);
+                cv.notify_one();
+            }
+        }
+        for thread in &mut self.threads {
+            if let Some(thread) = thread.take() {
+                thread.join().unwrap();
+            }
         }
     }
 }
