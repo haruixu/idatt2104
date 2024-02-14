f1582b173403dcbbde248856943cb5030b365573
haruixu <xulr0820@hotmail.com>
2024-02-14 14:55:05 +0100

    "Created asio classes"

---
+++ src/bin/asio_c.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 2;
+    let y = 3;
+
+    let z = x + y;
+}
---
+++ src/bin/asio_s.rs
@@ -0,0 +1,8 @@
+/*Gå igjennom koden fra leksjonen eller evt. se på et annet rammeverk/språk for ASIO. Lag en
+enkel kommunikasjonsløsning hvor tjener returnerer teksten klienten sender til tjeneren. Her
+kan telnet/netcat godt brukes som klient...*/
+use tokio::spawn;
+
+fn main() {
+    print!("hello main");
+}
