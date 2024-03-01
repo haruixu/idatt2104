85a1c84ce89c64fddf53e57c53dac8edecdf868c
haruixu <xulr0820@hotmail.com>
2024-03-01 06:45:17 +0100

    Hardcoded websocket client + started implementing handshake for server

---
+++ src/bin/assignment6/websocket-client.rs
@@ -0,0 +1,47 @@
+use std::{
+    io::Write,
+    net::{Ipv4Addr, SocketAddrV4, TcpStream},
+};
+
+fn main() -> Result<(), std::io::Error> {
+    //Strictly for mocking a websocket handshake
+    {
+        let to_socket_adr: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
+        let mut stream: TcpStream = TcpStream::connect(to_socket_adr)?;
+
+        let request_line: &str = "GET / HTTP/1.1\r\n";
+
+        let mut headers: Vec<String> = Vec::new();
+        add_header(&mut headers, "Host: ntnu.no");
+        add_header(&mut headers, "Upgrade: websocket");
+        add_header(&mut headers, "Connection: Upgrade");
+        add_header(&mut headers, "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==");
+        add_header(&mut headers, "Sec-WebSocket-Version: 13");
+
+        let request = create_request(request_line, headers);
+
+        println!("{request}");
+
+        stream.write_all(request.as_bytes())?;
+        stream.flush()?;
+    }
+
+    Ok(())
+}
+
+fn add_header(headers: &mut Vec<String>, header: &str) {
+    let crlf_header: String = format!("{}\r\n", header);
+    print!("{crlf_header}");
+    headers.push(crlf_header);
+}
+
+fn create_request(request_line: &str, headers: Vec<String>) -> String {
+    let mut request: String = String::from(request_line);
+
+    for header in &headers {
+        request.push_str(header.as_str());
+    }
+    request.push_str("\r\n");
+
+    request
+}
---
+++ src/bin/assignment6/websocket-server.rs
@@ -0,0 +1,79 @@
+use idatt2104::ThreadPool;
+use std::{
+    collections::BTreeMap,
+    io::{BufRead, BufReader},
+    net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream},
+};
+
+struct Websocket {
+    stream: TcpStream,
+    magic_string: String,
+}
+
+impl Websocket {
+    fn new(stream: TcpStream) -> Self {
+        let magic_string = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11".into();
+        Websocket {
+            stream,
+            magic_string,
+        }
+    }
+
+    fn handshake(&self) -> Result<(), std::io::Error> {
+        // Parse request
+        let reader = BufReader::new(self.stream.try_clone().unwrap());
+
+        let mut request_lines = reader.lines();
+
+        let request_line = request_lines.next().unwrap().unwrap();
+        let headers: BTreeMap<String, String> = request_lines
+            .take_while(|line| !line.as_ref().unwrap().is_empty())
+            .map(|header| {
+                let header = header.unwrap();
+                let (key, value) = header.split_once(": ").unwrap();
+                (key.to_string(), value.to_string())
+            })
+            .collect();
+
+        println!("reqlien:{request_line}");
+        println!("headers: {:#?}", headers);
+
+        let key = headers.get("Sec-WebSocket-Key".into());
+
+        //**handshake
+        // X Må reade request , Lese upgrade feltet
+        //Oppgradere til websocket - dekode keyen
+        //
+        //**Meldinger
+        //Demaskere data i body
+        Ok(())
+    }
+}
+fn handle_request(stream: TcpStream) {
+    let ws: Websocket = Websocket::new(stream.try_clone().unwrap());
+    match ws.handshake() {
+        Ok(()) => println!("Connection succeeded"),
+        Err(err) => println!("Connection failed: {}", err),
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let socket: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
+    let listener = TcpListener::bind(socket)?;
+    let pool = ThreadPool::new(5);
+
+    for stream in listener.incoming() {
+        pool.execute(|| handle_request(stream.unwrap()));
+    }
+
+    Ok(())
+}
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn base() {
+        assert_eq!(1, 1);
+    }
+}
